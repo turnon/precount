91108dca96ba8e3ba95a1c1ffb124cd08bedfe44
ken <block24block@gmail.com>
2018-05-27 22:17:45 +0800

    handle polymophic

--- lib/precount/collection.rb
+++ lib/precount/collection.rb
@@ -11,9 +11,17 @@ module Precount
     def joining_and_filter
       return @joining_and_filter if @joining_and_filter
       rel = any_record.association(asso).scope.unscope(:order)
-      i = rel.where_values.index{|w| Arel::Nodes::Equality === w && w.right == '$1' }
-      @full_fk_name = rel.where_values.delete_at(i).to_sql.sub! /\s*\=.*/, ''
+
+      p1_i = rel.where_values.index{|w| Arel::Nodes::Equality === w && w.right == '$1' }
+      @full_fk_name = rel.where_values.delete_at(p1_i).to_sql.sub!(/\s*\=.*/, '')
       rel.bind_values.pop
+
+      if p2_i = rel.where_values.index{|w| Arel::Nodes::Equality === w && w.right == '$2' }
+        type_column = rel.where_values.delete_at(p2_i).to_sql.sub!(/\s*\=.*/, '')
+        type_value = rel.bind_values.pop[1]
+        rel.where("#{type_column} = ?", type_value)
+      end
+
       @joining_and_filter = rel.where("#{@full_fk_name} IN (?)", bind_values(asso))
     end
 
